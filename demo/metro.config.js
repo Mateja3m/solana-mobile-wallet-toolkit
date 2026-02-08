c96c06e70e642c27ea081081aeae5c9f3e4e1350
Mateja3m <milanmatejicdev@gmail.com>
2026-02-08 17:49:41 +0100

    fix: restore stable metro monorepo resolution for release build

--- demo/metro.config.js
+++ demo/metro.config.js
@@ -1,15 +1,21 @@
 const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 const projectRoot = __dirname;
 const workspaceRoot = path.resolve(projectRoot, '..');
 
 const config = getDefaultConfig(projectRoot);
 
-config.watchFolders = [workspaceRoot];
+config.watchFolders = [path.resolve(workspaceRoot, 'toolkit')];
 config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
   path.resolve(workspaceRoot, 'node_modules')
 ];
-config.resolver.disableHierarchicalLookup = true;
+
+config.resolver.blockList = exclusionList([
+  new RegExp(`${path.resolve(workspaceRoot, '.git')}/.*`),
+  new RegExp(`${path.resolve(workspaceRoot, 'docs')}/.*`)
+]);
 
 module.exports = config;
