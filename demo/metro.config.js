f4fd9e41e3c701debc6a4b494fbfbcb3926fc1dc
Mateja3m <milanmatejicdev@gmail.com>
2026-02-08 17:29:31 +0100

    fix: simplify metro workspace resolver config

--- demo/metro.config.js
+++ demo/metro.config.js
@@ -1,22 +1,15 @@
 const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 const projectRoot = __dirname;
 const workspaceRoot = path.resolve(projectRoot, '..');
 
 const config = getDefaultConfig(projectRoot);
 
-config.watchFolders = [path.resolve(workspaceRoot, 'toolkit')];
+config.watchFolders = [workspaceRoot];
 config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
   path.resolve(workspaceRoot, 'node_modules')
 ];
-
-config.resolver.blockList = exclusionList([
-
-  new RegExp(`${path.resolve(workspaceRoot, '.git')}/.*`),
-  new RegExp(`${path.resolve(workspaceRoot, 'docs')}/.*`)
-]);
+config.resolver.disableHierarchicalLookup = true;
 
 module.exports = config;
